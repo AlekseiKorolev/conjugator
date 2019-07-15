d8dea2a5cf6ff9c796ede8d3e34ce40683aca049
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2019-07-15 07:00:40 +0200

    Updates

---
+++ precache-manifest.9e1eb028c5663cbf16b750ca1b23a41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc99dfe4066a79f51df690ed4431ffd7",
+    "url": "/conjugator/index.html"
+  },
+  {
+    "revision": "1420a9012ac2863473a2",
+    "url": "/conjugator/static/css/main.2967473d.chunk.css"
+  },
+  {
+    "revision": "8e3f261974bbadf705d7",
+    "url": "/conjugator/static/js/2.97d90857.chunk.js"
+  },
+  {
+    "revision": "1420a9012ac2863473a2",
+    "url": "/conjugator/static/js/main.4c3bf862.chunk.js"
+  },
+  {
+    "revision": "273a1a1ca9ac61a99cea",
+    "url": "/conjugator/static/js/runtime~main.f1cea6a4.js"
+  },
+  {
+    "revision": "7e3a4a6b36c67600be5e1c312cd9eeec",
+    "url": "/conjugator/static/media/bg.7e3a4a6b.png"
+  }
+]);
